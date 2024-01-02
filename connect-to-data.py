af5a211abb0f95fb6e890b2442c9b75f276f7687
Navya Agarwal <navya.agarwal.in@gmail.com>
2024-01-02 12:41:21 +0530

    dash app with data

---
+++ connect-to-data.py
@@ -0,0 +1,14 @@
+from dash import Dash, html, dash_table
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(children='My first Dash App with Data!'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
